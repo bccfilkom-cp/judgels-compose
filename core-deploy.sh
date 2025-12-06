c752ad833bf2a806fc57f54593ed9695a127a984
devanfer <ferrelvsc20@gmail.com>
2025-12-06 18:59:43 +0700

    fix: specify default judgels tag to fix broken config

--- core-deploy.sh
+++ core-deploy.sh
@@ -4,13 +4,24 @@
 mkdir -p ./judgels/client/var ./judgels/server/var
 chmod -R 777 ./judgels
 
+STATUS_COMPOSE=$(docker compose ls | grep "running");
+
+if [ ! -z "$STATUS_COMPOSE" ]; then 
+    echo "Compose is already running";
+    echo "Restarting compose...";
+    rm -rf judgels
+    docker compose down
+else 
+    echo "Starting judgels containers..."
+fi 
+
 # Spin up the Judgels Containers
 docker compose up -d
 
 # Check if the containers are running
 echo 'Waiting for Judgels Server to start...';
 sleep 5
-CONTAINER_COUNT=docker container ls | grep 'judgels-' | grep 'Up' | wc -l
+CONTAINER_COUNT=$(docker container ls | grep 'judgels-' | grep 'Up' | wc -l)
 
 if [ "$CONTAINER_COUNT" -ne 5 ]; then
     echo '-----------------------------------------------------------------------'
@@ -23,6 +34,7 @@ echo 'Judgels Server is Up and Running!';
 
 # Run the migration
 COMPOSE_NETWORK=judgels-compose_judgels-net
+IMAGE_VERSION="${JUDGELS_VERSION:-2.22.0}"
 
 echo "Running Judgels Server migration...";
 
@@ -31,5 +43,8 @@ docker run --rm \
     --network "$COMPOSE_NETWORK" \
     -v "./conf/judgels-server.yml:/judgels/server/var/conf/judgels-server.yml" \
     -v "./logs/judgels-server.log:/judgels/server/var/log/judgels-server.log" \
-    ghcr.io/ia-toki/judgels/server:latest \
+    "ghcr.io/ia-toki/judgels/server:${IMAGE_VERSION}" \
     db migrate
+
+echo "Judgels database migration success!"
+echo "Judgels server is already running!"
