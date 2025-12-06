29d54050d94005774ffea4b634eda4a7d6ea15dd
devanfer <ferrelvsc20@gmail.com>
2025-12-06 20:22:36 +0700

    fix(grader): fix grader spawn bash script

--- spawn-grader.sh
+++ spawn-grader.sh
@@ -7,17 +7,18 @@ for(( i=0; i<NUM_OF_GRADERS; i++ ))
 do
     echo "Creating judgels-grader-$i container....";
 
-    docker run \ 
+    docker run -d \
         --name judgels-grader-$i \
-        --network judgels-compose_judgels-net \
         --privileged \
-        -v ./judgels/grader/var:/judgels/grader/var \
+        -v ./judgels/grader-$i/var:/judgels/grader/var \
         -v ./conf/judgels-grader.yml:/judgels/grader/var/conf/judgels-grader.yml \
+        --health-cmd="pgrep -f judgels || exit 1" \
+        --restart on-failure:3 \
         --log-driver json-file \
         --log-opt max-size=256m \
         --log-opt max-file=2 \
         -e JUDGELS_GRADER_APP_OPTS="-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/judgels/grader/var/log" \
         "ghcr.io/ia-toki/judgels/grader:${IMAGE_VERSION}"
 
-    echo "Judgels-grader-$i container created....";
+    echo "judgels-grader-$i container created....";
 done 
