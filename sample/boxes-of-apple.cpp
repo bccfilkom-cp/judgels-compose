28b1e2dd2bcea26d363e456477e2a78eb2ac2fdd
devanfer <ferrelvsc20@gmail.com>
2026-04-20 02:23:18 +0700

    feat: add single vm support config

---
+++ sample/boxes-of-apple.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    long long sum = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        long long x;
+        cin >> x;
+        sum += x;
+    }
+    cout << sum << endl;
+    return 0;
+}
--- spawn-grader.sh
+++ spawn-grader.sh
@@ -2,6 +2,7 @@
 
 NUM_OF_GRADERS=2
 IMAGE_VERSION="${JUDGELS_VERSION:-2.22.0}"
+COMPOSE_NETWORK=judgels-compose_judgels-net
 
 for(( i=0; i<NUM_OF_GRADERS; i++ ))
 do
@@ -10,8 +11,10 @@ do
     docker run -d \
         --name judgels-grader-$i \
         --privileged \
+        --network $COMPOSE_NETWORK \
         -v ./judgels/grader-$i/var:/judgels/grader/var \
         -v ./conf/judgels-grader.yml:/judgels/grader/var/conf/judgels-grader.yml \
+        -v ./judgels/server/var/data:/judgels/server/var/data:ro \
         --health-cmd="pgrep -f judgels || exit 1" \
         --restart on-failure:3 \
         --log-driver json-file \
